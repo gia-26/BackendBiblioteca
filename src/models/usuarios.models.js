d47d6fb73bc539a61b61479a0268310c3c927c39
Giovanni <20240990@uthh.edu.mx>
2026-03-11 00:28:13 -0600

    validación de ejemplares

--- src/models/usuarios.models.js
+++ src/models/usuarios.models.js
@@ -2,12 +2,12 @@ import db from '../config/db.js';
 
 export const getUsuariosById = async ({id, tipo}) => {
     if (tipo === 'TU001') {
-        const [rows] = await db.query('SELECT Nombre, Id_alumno AS id FROM tbl_alumnos WHERE Id_alumno = ?', [id]);
-        return rows[0];
+      const [rows] = await db.query('SELECT Nombre, Id_alumno AS id FROM tbl_alumnos WHERE Id_alumno = ?', [id]);
+      return rows[0];
     }
     else if (tipo === 'TU002') {
-        const [rows] = await db.query('SELECT Nombre, Id_trabajador AS id FROM tbl_trabajadores WHERE Id_trabajador = ?', [id]);
-        return rows[0];
+      const [rows] = await db.query('SELECT Nombre, Id_trabajador AS id FROM tbl_trabajadores WHERE Id_trabajador = ?', [id]);
+      return rows[0];
     }
 }
 
