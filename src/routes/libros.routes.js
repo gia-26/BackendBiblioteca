842e84f820be95a110c02ac3c9b05709c9ad3006
Giovanni <20240990@uthh.edu.mx>
2026-03-21 16:38:56 -0600

    cambios api libros

--- src/routes/libros.routes.js
+++ src/routes/libros.routes.js
@@ -4,6 +4,7 @@ import * as ctrl from '../controllers/libros.controllers.js';
 const router = Router();
 
 router.get('/:id', ctrl.getLibrosById);
+router.get('/:id/subgeneros', ctrl.getSubgenerosByLibroId);
 
 router.post('/agregar', ctrl.agregarLibro)
 
