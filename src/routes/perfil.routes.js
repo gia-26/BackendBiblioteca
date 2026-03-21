515662a24da871a39b8fc163ad857004178164a5
Ivan Bautista Martinez <ivanbautistamartinez175@gmail.com>
2026-03-21 17:37:15 -0600

    perfil

--- src/routes/perfil.routes.js
+++ src/routes/perfil.routes.js
@@ -4,8 +4,8 @@ import { verificarToken } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.get('/:idUsuario', verificarToken, ctrl.getPerfil);
 router.put('/telefono', verificarToken, ctrl.updateTelefono);
 router.put('/password', verificarToken, ctrl.updatePassword);
+router.get('/:idUsuario', verificarToken, ctrl.getPerfil);
 
 export default router;
