8c8c1e5d8528e3aa78395fb86d467d3784bd6001
Giovanni <20240990@uthh.edu.mx>
2026-03-20 20:31:24 -0600

    api autores

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import editorialesRoutes from './routes/editoriales.routes.js';
 import tipoUsuariosRoutes from './routes/tipo_usuarios.routes.js';
 import loginRoutes from './routes/login.routes.js';
 import tiposPrestamoRoutes from './routes/tipos_prestamo.routes.js';
+import autoresRoutes from './routes/autores.routes.js';
 
 
 //Creamos el objeto de express para nuestra aplicación
@@ -55,6 +56,7 @@ app.use('/api/editoriales', editorialesRoutes);
 app.use('/api/tipo_usuarios', tipoUsuariosRoutes);
 app.use('/api/login', loginRoutes);
 app.use('/api/tipos_prestamo', tiposPrestamoRoutes);
+app.use('/api/autores', autoresRoutes);
 
 app.get('/',(req, res)=>{
     res.send("Esto es mi primer API desde express... publicada en vercel...");
