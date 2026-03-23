35ea1fa9bb5ace8241c809955a4d5df2b2eedef9
Giovanni <20240990@uthh.edu.mx>
2026-03-23 12:01:34 -0600

    estimacion api

--- src/models/estimacion.models.js
+++ src/models/estimacion.models.js
@@ -9,12 +9,12 @@ export const getAllLibrosEstimaciones = async () => {
             lib.Imagen
         FROM tbl_libros lib
         INNER JOIN tbl_ejemplares ej 
-            ON lib.Id_libro = ej.Id_libro
+        ON lib.Id_libro = ej.Id_libro
         INNER JOIN tbl_prestamos p 
-            ON ej.Id_ejemplar = p.Id_ejemplar
+        ON ej.Id_ejemplar = p.Id_ejemplar
         WHERE p.Fecha_prestamo > '2026-01-01'
         GROUP BY lib.Id_libro, lib.Titulo
-        ORDER BY lib.Titulo ASC;   
+        ORDER BY COUNT(p.Id_prestamo) DESC;    
     `);
 
     return rows;
