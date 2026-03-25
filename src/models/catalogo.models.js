dd276e8485b9c586008adac7e7f9f4ba1f60c75b
Giovanni <20240990@uthh.edu.mx>
2026-03-24 18:44:58 -0600

    getcatalgoo modificado

--- src/models/catalogo.models.js
+++ src/models/catalogo.models.js
@@ -11,11 +11,11 @@ export const getCatalogo = async (limit, skip) => {
                 ELSE lib.Sinopsis
             END AS Sinopsis,
             lib.Imagen,
-            COUNT(ejem.Id_ejemplar) AS Ejemplares_Disponibles
+            COUNT(ej.Id_ejemplar) AS Ejemplares_Disponibles
         FROM tbl_libros lib
         INNER JOIN tbl_autores aut ON lib.Id_autor = aut.Id_autor
-        LEFT JOIN tbl_ejemplares ejem ON lib.Id_libro = ejem.Id_libro 
-            AND ejem.Id_estado_ejemplar != 'EE002'
+        LEFT JOIN tbl_ejemplares ej ON lib.Id_libro = ej.Id_libro 
+            AND ej.Id_estado_ejemplar != 'EE002'
         WHERE lib.Estado != 0
         GROUP BY lib.Id_libro
         LIMIT ? OFFSET ?;  
