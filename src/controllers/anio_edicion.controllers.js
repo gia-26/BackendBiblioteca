96a498fb93e92a364828394ec74fe5aea3f23971
Ivan Bautista Martinez <ivanbautistamartinez175@gmail.com>
2026-03-24 21:15:57 -0600

    notificacion anio edicion

--- src/controllers/anio_edicion.controllers.js
+++ src/controllers/anio_edicion.controllers.js
@@ -32,6 +32,15 @@ export const editarAnio = async (req, res) => {
 export const eliminarAnio = async (req, res) => {
     try {
         const { Id_anio_edicion } = req.body;
+
+        //Verificar si está asignado a algún libro
+        const asignado = await anio_edicionModel.anioEstaAsignado(Id_anio_edicion);
+        if (asignado) {
+            return res.status(400).json({
+                error: "Este año de edición no se puede eliminar porque está asignado a uno o más libros."
+            });
+        }
+
         const result = await anio_edicionModel.deleteAnio(Id_anio_edicion);
         res.status(200).json(result);
     } catch (error) {
