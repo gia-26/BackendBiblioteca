18dd3a6a46ea9ee0b094597c4626cdb53140c3d2
Juanjo <20241026@uthh.edu.mx>
2026-03-25 02:42:44 -0600

    biblioteca js

--- src/models/usuarios.models.js
+++ src/models/usuarios.models.js
@@ -147,7 +147,7 @@ export const getPrestamosUsuario = async (idUsuario) => {
     SELECT 
       p.*, 
       l.Titulo AS titulo,
-      l.autor AS autor,
+      l.Id_autor AS autor,
       CASE 
         WHEN p.Id_estado_prestamo = 'EP001' THEN 'Activo'
         WHEN p.Id_estado_prestamo = 'EP002' THEN 'Entregado'
